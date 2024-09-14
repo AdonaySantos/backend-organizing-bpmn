af37338f9e8fbad5da1af733167b05ed6b5c26a2
Adonay <adonay.rssantos@gmail.com>
2024-09-13 22:08:02 -0300

    Correção

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 
 const app = express();
